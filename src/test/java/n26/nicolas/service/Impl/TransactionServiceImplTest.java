9c73cc48695c935bdf5702b9f8ad6209f4fa4025
Lecilufa-Kai <lecilufa@gmail.com>
2020-09-26 17:32:17 +0200

    update dependencies

---
+++ src/test/java/n26/nicolas/service/Impl/TransactionServiceImplTest.java
@@ -0,0 +1,14 @@
+package n26.nicolas.service.Impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TransactionServiceImplTest {
+
+    @Test
+    public void hi(){
+        System.out.println("Hello 123");
+    }
+}
